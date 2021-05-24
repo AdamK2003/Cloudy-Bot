10d8a8a585742b30f99e054fce9a76f93db4ce76
Max <maximebourbon562@gmail.com>
2021-05-24 18:05:36 +0200

    Update history.js
    a

--- commands/moderation/history.js
+++ commands/moderation/history.js
@@ -6,7 +6,6 @@ mongoose.connect(db)
 
 // const get = require('get-value')
 
-const Bans = require(process.env.ROOTDIR + '/models/bans.js')
 const Report = require(process.env.ROOTDIR + '/models/warn.js')
 const Mutes = require(process.env.ROOTDIR + '/models/mutes.js')
 
@@ -33,20 +32,6 @@ module.exports.run = async (client, message, args) => {
     .setColor("#ffff00")
     .setTitle("Punishements of " + rUser.user.tag);
 
-Bans.find({ userID: rUser.id, guildID: message.guild.id }, (err, arr) => {
-   if(arr.length == 0) {
-      embed.addField("**Bans**\nNothing to see here", '\u200b')
-  } else {
-      var i;
-      for(i = 0; i < arr.length; i++) {
-          embed.addField('\u200b', '\u200b')
-          embed.addField("**Bans**\nBanned by " + arr[i].BannedByID, "Reason: " + arr[i].reason)
-          embed.addFields(
-              { name: "Time", value: arr[i].time, inline: true },
-              { name: "Ban ID", value: arr[i]._id, inline: true }
-          )
-      }
-  }
 Report.find({ userID: rUser.id, guildID: message.guild.id }, (err, arr) => {
    if(arr.length == 0) {
       embed.addField("**Warns**\nNothing to see here", '\u200b')
@@ -76,6 +61,6 @@ Mutes.find({ userID: rUser.id, guildID: message.guild.id }, (err, arr) => {
       }
   }
 message.channel.send(embed)
-})})})
+})})
 return
 }
