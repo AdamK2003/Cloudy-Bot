cdd6ce41ccb741d87f734e90f32a90d9e5b2f60b
Max <maximebourbon562@gmail.com>
2021-05-26 11:47:02 +0200

    Update crr.js

--- commands/server/crr.js
+++ commands/server/crr.js
@@ -1,19 +1,25 @@
 module.exports.run =(client, message, args) => {
+   message.delete();
 
-   let roleName = args.slice(0).join(' ');
-   let roleC = message.guild.roles.cache.find(r => r.name === roleName);
+   let roleName = args.slice(0).join(' ')
+   let roleC = message.guild.roles.cache.find(r => r.name === roleName)
 
    let emote =  args.slice(1).join(' ');
 
+   if(!message.guild.roles.cache.find(r => r.name === roleName)) { message.channel.send("Invalid role!") };
    if(!roleName) return(message.channel.send(`Please specify a role name.Be careful to cap letter,this command is case sinsitive.`));
    if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send('You can\'t use that!');
      if(!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send('I don\'t have the right permissions.');
      if(message.member.roles.highest.position < roleC) {
       message.channel.send('You do not have a high enough role to add this role to a react role message.'); }
 
-   message.channel.send(`React to this message with ${emote} to get the ${roleName} role!`).then(async m => { 
+   message.channel.send(`React to this message with \`${emote}\` to get the \`${roleName}\` role!`).then(async m => { 
       m.react(emote);
    })
+
+   if (member.react) {
+      member.roles.add(role)
+   }
 };
 
 module.exports.help = {
