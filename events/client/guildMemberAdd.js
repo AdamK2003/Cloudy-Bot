517176ae5db4c4c0e69c1f7a1c1647728221837a
Max <maximebourbon562@gmail.com>
2021-05-29 20:05:15 +0200

    Update guildMemberAdd.js

--- events/client/guildMemberAdd.js
+++ events/client/guildMemberAdd.js
@@ -5,7 +5,7 @@ module.exports = (client, member) => {
             .setColor("RANDOM")
             .setDescription(`Welcome ${member.displayName} to the server!`)
             .setThumbnail(user_mention.avatarURL({format: "png", dynamic: true, size: 128}))
-            .setFooter(`User id: ${member.id}`)
+            .setFooter(`User ID: ${member.id}`)
             .setTimestamp();
             
             client.channels.cache.get('846084493184860172').send(embed);
