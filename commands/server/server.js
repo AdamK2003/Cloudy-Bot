d82f727094ed0b6f4032be86cecd8d5c140584bf
Max <maximebourbon562@gmail.com>
2021-05-30 21:00:50 +0200

    Update server.js

--- commands/server/server.js
+++ commands/server/server.js
@@ -7,8 +7,8 @@ module.exports.run =(client, message, args) => {
             .setTitle(message.guild.name, message.guild.icon)
             .addField(`Server created the: `, ` ${message.guild.createdAt}`, true)
             .addField(`Member: `, message.guild.memberCount, true)
-            .addField(`\nNumber of text channels `, message.guild.channels.filter((c) => c.type === "text").size, true)
-            .addField(`\nNumber of voice channels `, message.guild.channels.filter((c) => c.type === "voice").size, true)
+            //.addField(`\nNumber of text channels `, message.guild.channels.filter((c) => c.type === "text").size, true)
+            //.addField(`\nNumber of voice channels `, message.guild.channels.filter((c) => c.type === "voice").size, true)
             .setImage(message.guild.icon({format: "png", dynamic: true, size: 512}))
             .setFooter(`Server ID: ${message.guild.id}`)
            
@@ -17,10 +17,10 @@ module.exports.run =(client, message, args) => {
       
     
 module.exports.help = {
-         name: "info",
-         aliases: ['information', 'user'],
+         name: "server",
+         aliases: ['server', 's'],
          category: 'server',
-         description: "give info about a user",
+         description: "Give information about the server.",
          usage: "(member)",
          cooldown: 10,
          args: false
