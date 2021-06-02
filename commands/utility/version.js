7c8deb3b06b857c2a36ac40a1e39bc4b67a31f14
Max <maximebourbon562@gmail.com>
2021-06-02 20:34:21 +0200

    Update version.js

--- commands/utility/version.js
+++ commands/utility/version.js
@@ -5,7 +5,7 @@ module.exports.run =(client, message, args) => {
    var serverIcon = message.guild.iconURL();
 
    const embed = new MessageEmbed()
-   .addDescription(`**-----Version Calendar-----**`)
+   .setTitle(`**-----Version Calendar-----**`)
    .addField(`Alpha:`, `1.0: Start\n1.1: Mod fully done\n1.2: server + mod fully done\n1.4: server + mod + utility`, true)
    .addField(`Beta`,`2.0: Config bot done\n2.5: Fixing every bug\n2.7: Design / Make the bot better looking`, true)
    .addField(`Public`,`1.0: everything done + make a report bug command\n1.5: new commands\n2.0: currency system`, true)
