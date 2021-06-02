499257780bc335800f019c18aafcc0fad61d3a85
Max <maximebourbon562@gmail.com>
2021-06-02 21:43:44 +0200

    Update guildMemberAdd.js

--- events/client/guildMemberAdd.js
+++ events/client/guildMemberAdd.js
@@ -1,9 +1,8 @@
 const { MessageEmbed, Message, Guild } = require("discord.js");
 
 module.exports = (client, member) => {
-  const embed = new MessageEmbed()
             
-            client.channels.cache.get('846084493184860172').send(`Welcome ${member} to ${guild.name}`);
+            client.channels.cache.get('846084493184860172').send(`Welcome ${member} to ${guild.name}!`);
 }
 
 // would be cool in he future to be able to edit the welcome message on a specific guild.
